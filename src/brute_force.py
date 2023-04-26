f4d9adc7cd0d82d485b8ce31ccede94b83754a41
HamadAbdulRazzaq <hamadarazzaq@gmail.com>
2023-04-27 02:13:57 +0500

    Brute Force Updated

--- src/brute_force.py
+++ src/brute_force.py
@@ -1,60 +1,110 @@
 from random import randint
+import numpy as np
+from time import perf_counter
+import matplotlib.pyplot as plt
+# def permute(all_lst: list, lst: list, l: int, r: int, n:int, G:dict): # Find all graphs with node "n" that makes up the hamiltonian cycle
+#     if l == r:
+#         all_lst.append(lst.copy())
+#         return BruteForce_HamiltonianCycle(G, n, lst)
+#     else:
+#         for i in range(l,r):
+#             lst[l], lst[i] = lst[i], lst[l]
+#             if permute(all_lst, lst, l+1, r, n, G) == True:
+#                 return True
+#             lst[l], lst[i] = lst[i], lst[l]
 
-def permute(all_lst: list, lst: list, l: int, r: int, n:int):
-    if l == r:
-        all_lst.append(lst.copy())
-    else:
-        for i in range(l,r):
-            lst[l], lst[i] = lst[i], lst[l]
-            permute(all_lst, lst, l+1, r, n)
-            lst[l], lst[i] = lst[i], lst[l]
 
-
-def Generate_Graph(type: int):
-    if type == 0:
-        for n in range(1,12):
-            G = {i: [] for i in range(1,n+1)}
-            for j in range(1, n+1):
-                for k in range(j+1, n+1):
-                    if (k not in G[j]):
-                        G[j].append(k)
-                        G[k].append(j)
-    elif type == 1:
-        for n in range(1,12):
-            G = {i: [] for i in range(1,n+1)}
-            for j in range(1, n+1):
-                for k in range(j+1, n+1):
-                    if randint(0,1) == 1:
-                        if (k not in G[j]):
-                            G[j].append(k)
-                            G[k].append(j)
-    return G
+# def Generate_Graph(type: int, V:int):
+#     G = dict()
+#     if type == 0: # Generate complete graph (best case)
+#         for n in range(1,V+1):
+#             G = {i: [] for i in range(1,n+1)}
+#             for j in range(1, n+1):
+#                 for k in range(j+1, n+1):
+#                     if (k not in G[j]):
+#                         G[j].append(k)
+#                         G[k].append(j)
+#     elif type == 1: # Generate random graph (average/worst case)
+#         for n in range(1,V+1):
+#             G = {i: [] for i in range(1,n+1)}
+#             for j in range(1, n+1):
+#                 for k in range(j+1, n+1):
+#                     if randint(0,1) == 1:
+#                         if (k not in G[j]):
+#                             G[j].append(k)
+#                             G[k].append(j)
+#     return G
     
 
 
-def BruteForce_HamiltonianCycle(G: dict, n: int):
-        all_combinations = []
-        lst = list(range(2, n+1))
-        permute(all_combinations, lst, 0, n-1, n)
-        Path_exist = True
-        for lt in range(len(all_combinations)):
-            all_combinations[lt].insert(0, 1)
-            for j in range(1, len(all_combinations[lt])+1):
-                if j == len(all_combinations[lt]) and j in G[1]:
-                    break
-                elif j+1 not in G[j]:
-                    Path_exist = False
-                    break
-            if Path_exist:
-                print("Hamiltonian Cycle Found !")
-                print(all_combinations[lt])
-                print(G)
-                break
-        print("No Hamiltonian Cycle Exists! for n =", n)
+# def BruteForce_HamiltonianCycle(G: dict, n: int, lst:list): # check if the edges in a permutation is present in the actual graph or not
+#         # all_combinations = []
+#         # lst = list(range(2, n+1))
+#         # permute(all_combinations, lst, 0, n-1, n)
+#     Path_exist = True
+#     lst.insert(0, 1)
+#     for j in range(1, len(lst)+1):
+#         if j == len(lst) and j in G[1]:
+#             break
+#         elif j+1 not in G[j]:
+#             Path_exist = False
+#             break
+#     if Path_exist:
+#         # print("Hamiltonian Cycle Found !")
+#         # print(lst)
+#         # print(G)
+#         return True
+#     return False
 
 
-for n in range(1,11):
-    G = Generate_Graph(0)
-    BruteForce_HamiltonianCycle(G, n)
-    G = Generate_Graph(1)
-    BruteForce_HamiltonianCycle(G, n)
+# V_l = 1
+# V_u = 6
+# cg = []
+# rg = []
+# for n in range(V_l,V_u+1):
+#     all_combinations = []
+#     lst = list(range(2, n+1))
+#     lst2 = lst.copy()
+#     G = Generate_Graph(0, n)
+#     a = perf_counter()
+#     if permute(all_combinations, lst, 0, n-1, n, G):
+#         print("Cycle Found! for n =", n, " (Complete Graph)")
+#     else:
+#         print("Cycle Not Found! for n =", n, " (Complete Graph)")
+#     b = perf_counter()
+#     cg.append(b-a)
+#     G = Generate_Graph(1, n)
+#     a = perf_counter()
+#     if permute(all_combinations, lst2, 0, n-1, n, G):
+#         print("Cycle Found! for n =", n, " (Randomized Graph)")
+#     else:
+#         print("Cycle Not Found! for n =", n, " (Randomized Graph)")
+
+#     b = perf_counter()
+#     rg.append(b-a)
+# print(cg)
+# print(rg)
+
+# cgf = [0.0005448000001706532, 0.0015295000002879533, 0.0012784000000465312, 0.0012435999997251201, 0.0014826999999968393, 0.0014458999999078515, 0.003106099999968137, 0.002625600000101258, 0.002559599999585771]
+# rgf = [0.0004967999998370942, 0.00045560000035038684, 0.0004473999997571809, 0.0004546000000118511, 0.0006035999999767228, 0.0009657999999035383, 0.004682999999658932, 0.12895280000020648, 7.954753600000004]
+# plt.plot(list(range(1,len(cgf)+1)), cgf)
+# plt.plot(list(range(1,len(cgf)+1)), rgf)
+# plt.legend(["Best Case", "Worst Case"])
+# plt.show()
+a = np.array([1, 2, 3, 4, 5, 6])
+b = np.array([7, 8, 9, 10, 11])
+c = np.array([13, 14, 15, 16, 17, 18])
+# X, Y = np.meshgrid(a, b)
+X = np.array( [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]] )
+Y = np.array( [[7 for _ in range(6)], [8 for _ in range(6)], [9 for _ in range(6)], [10 for _ in range(6)], [11 for _ in range(6)]] )
+print(X)
+print(Y)
+Z = np.sin(np.sqrt(X ** 2 + Y ** 2))
+print(Z)
+ax = plt.axes(projection = "3d")
+# ax.plot3D(a, b, c)
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                cmap='viridis', edgecolor='none')
+ax.set_title('Surface')
+# X = np.array( [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]] )
+plt.show()
---
+++ src/brute_force_updated.py
@@ -0,0 +1,94 @@
+from random import randint
+from time import perf_counter
+import numpy as np
+import matplotlib.pyplot as plt
+# np.set_printoptions(precision=3)
+
+def permute_and_check(all_lst: list, lst: list, l: int, r: int, n:int, G:dict) -> bool: # Find all graphs with node "n" that makes up the hamiltonian cycle
+    if l == r:
+        all_lst.append(lst.copy())
+        return HC_Check(G, n, lst)
+    else:
+        for i in range(l,r):
+            lst[l], lst[i] = lst[i], lst[l]
+            if permute_and_check(all_lst, lst, l+1, r, n, G) == True:
+                return True
+            lst[l], lst[i] = lst[i], lst[l]
+    return False
+
+
+def Generate_Graph(n: int, d: int) -> dict:
+    lb = 0
+    ub = n*(n-1)/2
+    p = d*(ub+1)
+    G = {i: [] for i in range(1,n+1)}
+    for j in range(1, n+1):
+        for k in range(j+1, n+1):
+            rnd = randint(lb, ub)
+            if (k not in G[j]):
+                if rnd < p:    
+                    G[j].append(k)
+                    G[k].append(j)
+    return G
+
+def HC_BF(G: dict, n: int) -> bool:
+    all_combinations = list()
+    lst = list(range(2, n+1))
+    return permute_and_check(all_combinations, lst, 0, n-1, n, G)
+
+def HC_Check(G: dict, n: int, lst:list):
+    Path_exist = True
+    lst.insert(0, 1)
+    for j in range(1, len(lst)+1):
+        if j == len(lst) and j in G[1]:
+            break
+        elif j+1 not in G[j]:
+            Path_exist = False
+            break
+    if Path_exist:
+        return True
+    return False
+
+def driver_code() -> None:
+    vlb = 1
+    vub = 6
+    f = dict()
+    x = list(range(vlb, vub+1))
+    y = [round(0.05*i, 2) for i in range(0,21)]
+    max = 0
+    for v in x:
+        for d in y:
+            total_time = 0
+            total_iter = 100
+            for _ in range(total_iter):
+                G = Generate_Graph(v, d)
+                start_time = perf_counter()
+                HC_BF(G, v)
+                stop_time = perf_counter()
+                total_time += (stop_time - start_time)
+            f[(v, d)] = total_time/total_iter
+            if f[(v, d)] > max:
+                max = f[(v, d)]
+    print(max)
+    X = np.array(list(list(i for i in range(vlb, vub+1)) for _ in range(len(y))))
+    Y = np.array(list(list(i for j in range(vlb, vub+1)) for i in y))
+    Z = np.array(list(list(0.0005 for _ in range(vlb, vub+1)) for _ in range(len(y))))
+    # print(f)
+    for i in range(len(y)):
+        for j in range(len(x)):
+            Z[i][j] = f[(X[i][j], Y[i][j])]
+    # print(X)
+    # print(Y)
+    # print(Z)
+    fig = plt.figure()
+    ax = plt.axes(projection = '3d')
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none')
+    ax.set_title("Hamiltoniain Cycle - Brute Force")
+    ax.invert_xaxis()
+    ax.set_xlabel("Number of Vertices")
+    ax.set_ylabel("Graph Density")
+    ax.set_zlabel("Time Taken")
+    plt.show()
+    
+driver_code()
+    
